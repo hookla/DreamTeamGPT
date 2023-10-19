6dd142e5c0573ad745ec8aaf6ccf5ce48af675b4
laurencehook <laurence_hook@hotmail.com>
2023-10-19 20:38:46 +0100

    rename some things

--- agents/agent.py
+++ agents/agent.py
@@ -12,7 +12,7 @@ openai.api_key = api_key
 class Agent:
     def __init__(self, name: str):
         self.name = name
-        self.common_instructions = "For the duration of this conversation, provide raw, concise opinions without any politeness or filler words. Focus on progressing the solution in a tangible way. Avoid generic statements and repetition of points already made. Ensure all comments are directly relevant to the conversation. If you cannot meet these criteria, respond with "no comment."
+        self.common_instructions = "For the duration of this conversation, provide raw, concise opinions without any politeness or filler words. Focus on progressing the solution in a tangible way. Avoid generic statements and repetition of points already made. Ensure all comments are directly relevant to the conversation. If you cannot meet these criteria, respond with 'no comment.'
 
     def query_gpt(self, prompt: str, max_tokens: int) -> str:
         final_prompt = f"{self.common_instructions} {prompt}"
