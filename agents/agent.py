31b794a799ad81e6f78e16de7be03c83cbda3abf
laurence.hook <laurence.hook@revolut.com>
2023-10-21 21:34:18 +0100

    tracks how often someone has spoken so that the chairman doesnt leave anyone out

--- agents/agent.py
+++ agents/agent.py
@@ -2,21 +2,18 @@ from textwrap import dedent
 
 from gpt.gpt_client import GPTClient
 
+DEFAULT_SYSTEM_PROMPT = dedent("""\
+Provide succinct, fact-based answers. Eliminate filler words and politeness. 
+Concentrate on delivering actionable insights and concrete solutions.
+Avoid vague or generic statements. Stick to the topic at hand. 
+#      If you response doesn't meet these standards, reply with the exact phrase 'no comment'
+"""
+)
 
 class Agent:
-    def __init__(self, name: str, user_prompt: str):
+    def __init__(self, name: str, user_prompt: str, system_prompt: str = DEFAULT_SYSTEM_PROMPT):
         self.name = name
-        
-        self.common_instructions = dedent(
-        """\
-        Provide succinct, fact-based answers. Eliminate filler words and politeness. 
-        Concentrate on delivering actionable insights and concrete solutions.
-        Avoid vague or generic statements. Stick to the topic at hand. 
-        If the query doesn't meet these standards, reply with 'no comment.'
-        """
-        )
-
-        self.gpt_client = GPTClient(self.common_instructions, user_prompt)
+        self.gpt_client = GPTClient(system_prompt, user_prompt)
 
     def query_gpt(self, transcript: str) -> str:
         return self.gpt_client.query(transcript)
--- agents/chairman.py
+++ agents/chairman.py
@@ -3,34 +3,42 @@ from loguru import logger
 from agents.agent import Agent
 from agents.sme import SME
 
-logger.disable(__name__)
+
+# logger.disable(__name__)
 
 
 class Chairman(Agent):
-    def __init__(self, name: str, executives: list):
+    def __init__(self, name: str, SMEs: list[SME]):
         # Construct the user_prompt string with details of the executives
-        exec_details = ""
-        for executive_agent in executives:
-            exec_details += (
-                f"{executive_agent.name}: expert in {executive_agent.expertise} "
-                f"and concerned about {', '.join(executive_agent.concerns)}.\n"
-            )
 
-        user_prompt = (
-            f"Your task is to decide who should speak next among meeting participates. "
-            f"Answer with only the name and nothing else. "
-            f"Do not call on the same person too often.\nParticipants: {exec_details}"
+        self.user_prompt = self.update_user_prompt(SMEs)
+
+        system_prompt = (
+            f"Answer with only the name and nothing else."
         )
 
         # Call the superclass constructor with the constructed user_prompt
-        super().__init__(name, user_prompt)
+        super().__init__(name, self.user_prompt, system_prompt)
 
-        self.executives = executives
+        self.SMEs = SMEs
+
+    def update_user_prompt(self, SMEs: list[SME]) -> str:
+        frequency_info_list = []
+        for SME in SMEs:
+            frequency_info_list.append(
+                f"{SME.name}: expertise: {SME.expertise}. "
+                f"concerns: {', '.join(SME.concerns)}. spoken count: {SME.spoken_count}.\n"
+            )
+
+        return (
+            f"Your task is to read the transcript and decide who should speak next.  Do not choose the same person all of the time.\n"
+            f"Participants:\n{''.join(frequency_info_list)} "
+        )
 
     def decide_if_meeting_over(self, transcript: list) -> bool:
         return False
 
-    def decide_next_speaker(self, transcript_list: list) -> SME:
+    def decide_next_speaker(self, transcript_list: list[str]) -> SME:
         transcript = " ".join(transcript_list)
 
         while True:
@@ -38,10 +46,13 @@ class Chairman(Agent):
             logger.info(f"Chairman called speaker: {next_speaker}")
 
             next_executive = next(
-                (exec for exec in self.executives if exec.name == next_speaker), None
+                (exec for exec in self.SMEs if exec.name == next_speaker), None
             )
 
             if next_executive is not None:
+                next_executive.spoken_count += 1  # Update the frequency count
+                self.user_prompt = self.update_user_prompt(self.SMEs)
+                self.gpt_client.user_prompt = self.user_prompt
                 return next_executive
 
             logger.info(f"{next_speaker} is not a valid exec...")
