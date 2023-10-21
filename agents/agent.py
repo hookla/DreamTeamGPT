04d75a1aae7f0aa23e4c1655750d6b406942b73d
Laurence Hook <laurence_hook@hotmail.com>
2023-10-21 15:42:41 +0100

    Update agent.py
    Updated prompt

--- agents/agent.py
+++ agents/agent.py
@@ -6,16 +6,16 @@ from gpt.gpt_client import GPTClient
 class Agent:
     def __init__(self, name: str, user_prompt: str):
         self.name = name
+        
         self.common_instructions = dedent(
-            """\
-            For the duration of this conversation, provide raw, concise opinions 
-            without any politeness or filler words. 
-            Focus on progressing the solution in a tangible way. 
-            Avoid generic statements and repetition of points already made. 
-            Ensure all comments are directly relevant to the conversation. 
-            If you cannot meet these criteria, respond with 'no comment.'
-            """
+        """\
+        Provide succinct, fact-based answers. Eliminate filler words and politeness. 
+        Concentrate on delivering actionable insights and concrete solutions.
+        Avoid vague or generic statements. Stick to the topic at hand. 
+        If the query doesn't meet these standards, reply with 'no comment.'
+        """
         )
+
         self.gpt_client = GPTClient(self.common_instructions, user_prompt)
 
     def query_gpt(self, transcript: str) -> str:
